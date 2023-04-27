ba01cd687918dd5ca640e4217d71c93f15f92aa1
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-04-27 13:51:04 +0200

    install react-router and set paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="App">exploringSA</div>;
-}
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/AboutUs";
+import OurServices from "./routes/OurServices";
+import Contact from "./routes/Contact";
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/services" element={<OurServices />} />
+    <Route path="/contact" element={<Contact />} />
+  </Routes>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
