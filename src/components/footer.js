f6bd5a8f5432535972134443f62696cfbddfb509
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-04-27 19:57:54 +0200

    add content to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-const footer = () => (
-  <div>
-    <h1>Footer</h1>
-  </div>
-);
+const footer = () => {
+  return (
+    <div className="bg-bluebg p-10 flex justify-around">
+      <div>
+        <p>Singh's Farm</p>
+        <p>16 Schaap Road</p>
+        <p>Schaapkraal</p>
+        <p>Cape Town, 7941</p>
+        <p>Phone: +27 21 396 1145</p>
+        <p className="flex items-center gap-3">
+          Social Media:
+          <img
+            className="w-8"
+            src="/images/icons/facebook.svg"
+            alt="facebook logo"
+          />
+          <img
+            className="w-8"
+            src="/images/icons/whatsapp.svg"
+            alt="facebook logo"
+          />
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT US</li>
+          <li>SERVICES</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default footer;
