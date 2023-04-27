c63d7ba1cc3951a45e8318f2351c619ca6221e8f
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-04-27 16:16:42 +0200

    update menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  AiOutlineMenuFold,
+  AiOutlineMenuUnfold,
+} from "react-icons/ai";
 
-const Navbar = () => (
-  <nav className="flex justify-around items-center">
-    <div>
-      <h2>Exploring SA</h2>
-      <p>Travel and Tours</p>
-    </div>
-    <div>
-      <ul className="flex">
-        <li className="p-2">Home</li>
-        <li className="p-2">About Us</li>
-        <li className="p-2">Services</li>
-        <li className="p-2">Contact</li>
-      </ul>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [active, setActive] = useState("false");
+  return (
+    <nav className="flex items-center bg-bluebg w-screen text-[#DCDCDC]">
+      <div className="p-2 flex items-center">
+        <h2 className="text-2xl">Exploring SA</h2>
+        <p className="text-md ml-1 mt-1 ">Travel and Tours</p>
+      </div>
+
+      <div className="ml-auto mr-2 cursor-pointer">
+        <AiOutlineMenuUnfold color="#DCDCDC" size={30} />
+      </div>
+
+      <div
+        className={
+          !active
+            ? "hidden"
+            : "absolute w-full h-full top-0 text-blueText text-4xl bg-lightGreen flex"
+        }
+      >
+        <div className="right-2 top-2 absolute cursor-pointer">
+          <AiOutlineMenuFold /*color="#DCDCDC"*/ size={30} />
+        </div>
+        <ul className="w-fit m-auto">
+          <li className="py-12 ">Home</li>
+          <li className="py-12 ">About Us</li>
+          <li className="py-12 ">Services</li>
+          <li className="py-12 ">Contact</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      blue: "#172B4D",
+      bluebg: "#172B4D",
       lightGreen: "#D4F6CC",
       gray: "#2D334A",
       orange: "#EF5B0C",
       offWhite: "#F5F5F5",
       darkGreen: "#04420C",
-      blue: "#272343",
+      blueText: "#272343",
     },
     extend: {},
   },
