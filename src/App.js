3464cf8e94d7e925127ee75c6bce36102fedf359
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-03 20:13:22 +0200

    update project

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Home from "./routes/Home";
 import About from "./routes/AboutUs";
 import OurServices from "./routes/OurServices";
 import Contact from "./routes/Contact";
-import Footer from "./components/footer";
-import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 const App = () => (
   <>
