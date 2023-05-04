9bcad22e3ffb0477d2996cf2366a7228f069ef8f
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-04 15:54:44 +0200

    update content for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import {
 } from "react-icons/ai";
 
 const Navbar = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(true);
   return (
     <nav
       className="bg-bluebg w-full flex p-4 text-[#DCDCDC]"
@@ -14,18 +14,56 @@ const Navbar = () => {
       <div className="flex">
         {/* TITLE */}
         <div className="flex">
-          <h2>Exploring SA</h2>
-          <p>Travel and Tours</p>
+          <h2 className="text-xl font-bold">Exploring SA</h2>
+          <p className="ml-1 pt-[7px] text-sm">Travel and Tours</p>
         </div>
 
         {/* MENU LINKS */}
         {/* LINKS */}
-        <div className="hidden">
-          <ul>
-            <li>HOME</li>
-            <li>ABOUT US</li>
-            <li>SERVICES</li>
-            <li>CONTACT</li>
+        <div
+          className={
+            isActive
+              ? "w-screen absolute top-0 left-0 h-screen bg-lightGreen z-10 text-blueText flex justify-center items-center"
+              : "hidden"
+          }
+        >
+          <ul className={isActive ? "" : ""}>
+            <li
+              className={
+                isActive
+                  ? " my-14 text-4xl font-bold text-center"
+                  : "null"
+              }
+            >
+              HOME
+            </li>
+            <li
+              className={
+                isActive
+                  ? " my-14 text-4xl font-bold text-center"
+                  : "null"
+              }
+            >
+              ABOUT US
+            </li>
+            <li
+              className={
+                isActive
+                  ? " my-14 text-4xl font-bold text-center"
+                  : "null"
+              }
+            >
+              OUR SERVICES
+            </li>
+            <li
+              className={
+                isActive
+                  ? " my-14 text-4xl font-bold text-center"
+                  : "null"
+              }
+            >
+              CONTACT
+            </li>
           </ul>
         </div>
       </div>
