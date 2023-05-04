5d38785059211b0b5377f398b908e84452b126cd
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-04 18:52:30 +0200

    add functionality to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,55 +13,46 @@ const Navbar = () => {
     >
       <div className="flex">
         {/* TITLE */}
-        <div className="flex">
-          <h2 className="text-xl font-bold">Exploring SA</h2>
-          <p className="ml-1 pt-[7px] text-sm">Travel and Tours</p>
-        </div>
+
+        <h2
+          className={
+            isActive
+              ? "text-blueText font-bold z-20 text-xl"
+              : "text-xl font-bold"
+          }
+        >
+          Exploring SA
+        </h2>
+        <p
+          className={
+            isActive
+              ? "text-blueText font-bold z-20 text-sm pt-[7px] ml-1"
+              : "ml-1 pt-[7px] text-sm"
+          }
+        >
+          Travel and Tours
+        </p>
 
         {/* MENU LINKS */}
         {/* LINKS */}
         <div
           className={
             isActive
-              ? "w-screen absolute top-0 left-0 h-screen bg-lightGreen z-10 text-blueText flex justify-center items-center"
-              : "hidden"
+              ? "w-screen fixed left-0 top-0 h-screen bg-lightGreen z-10 text-blueText flex justify-center items-center ease-in duration-300"
+              : "w-full  absolute top-0 left-[-100%] h-full bg-lightGreen z-10 text-blueText flex justify-center items-center ease-in-out duration-700"
           }
         >
           <ul className={isActive ? "" : ""}>
-            <li
-              className={
-                isActive
-                  ? " my-14 text-4xl font-bold text-center"
-                  : "null"
-              }
-            >
+            <li className=" my-14 text-4xl font-bold text-center">
               HOME
             </li>
-            <li
-              className={
-                isActive
-                  ? " my-14 text-4xl font-bold text-center"
-                  : "null"
-              }
-            >
+            <li className=" my-14 text-4xl font-bold text-center">
               ABOUT US
             </li>
-            <li
-              className={
-                isActive
-                  ? " my-14 text-4xl font-bold text-center"
-                  : "null"
-              }
-            >
+            <li className=" my-14 text-4xl font-bold text-center">
               OUR SERVICES
             </li>
-            <li
-              className={
-                isActive
-                  ? " my-14 text-4xl font-bold text-center"
-                  : "null"
-              }
-            >
+            <li className=" my-14 text-4xl font-bold text-center">
               CONTACT
             </li>
           </ul>
@@ -69,7 +60,7 @@ const Navbar = () => {
       </div>
       {/* MENU BUTTON OPEN */}
       <div
-        className="absolute right-2 top-3"
+        className={isActive ? "hidden" : "absolute right-2 top-3"}
         onClick={() => setIsActive(!isActive)}
       >
         <AiOutlineMenuUnfold color="#DCDCDC" size={30} />
@@ -77,7 +68,11 @@ const Navbar = () => {
 
       {/* CLOSE MENU BUTTON */}
       <div
-        className={isActive ? "absolute right-2 top-3" : "hidden"}
+        className={
+          isActive
+            ? "absolute right-2 top-3 text-blueText z-20"
+            : "hidden"
+        }
         onClick={() => setIsActive(!isActive)}
       >
         <AiOutlineMenuFold size={30} />
