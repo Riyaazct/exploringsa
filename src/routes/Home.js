82833c145767fd6fb9fe05fb13d5db754c1a2a76
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-06 18:25:10 +0200

    complete home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,28 +3,31 @@ import React from "react";
 const bgPath = "/images/home.jpg";
 
 const Home = () => (
-  <div>
+  <div className="md:flex">
     <div className="relative">
       {/* TEXT CONTENT & BUTTON */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-offWhite text-center z-10 w-full">
-        <h1 className="font-semibold text-[1.2rem]">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-offWhite text-center z-10 w-full max-w-[85%]">
+        <h1 className="font-semibold text-[1.2rem] 2xl:text-3xl xl:text-2xl ">
           Welcome to Exploring SA Transport and Tours, your premier
           transportation and tourism provider in South Africa
         </h1>
-        <button className="mt-4 text-lg opacity-80 hover:opacity-100 text-offWhite border-offWhite hover:transform hover:scale-105 hover:duration-700 w-[130px]">
+        <button className="p-2 mt-4 text-lg opacity-80 hover:opacity-100 text-blueText font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold">
           Explore our Services
         </button>
       </div>
       {/* IMAGE */}
-      <div>
-        <img src={bgPath} alt="" className="object-fill" />
-        <div className="w-full h-full bg-[#000000a7] absolute top-0"></div>
+      <div className="relative">
+        <img
+          src={bgPath}
+          alt="Company vehicle"
+          className="max-h-[90vh] min-w-full object-fill xl:min-w-[45vw] xl:max-h-[92vh]"
+        />
+        <div className="w-full h-full bg-[#000000a7] absolute top-0 xl:min-w-[45vw]"></div>
       </div>
     </div>
-
-    <h1 className="text-center my-12 text-2xl text-blueText font-semibold">
-      "Unlock the treasures of South Africa with us"
-    </h1>
+    <div className="text-center my-12 text-2xl text-blueText font-semibold md:flex md:items-center md:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[45vw]">
+      <h1>"Unlock the treasures of South Africa with us"</h1>
+    </div>
   </div>
 );
 
