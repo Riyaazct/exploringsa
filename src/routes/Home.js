67f38db1221fd50b36e5ffbf38fa771ddcaee548
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-06 20:18:27 +0200

    make new adjustments to layout and styling for home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -19,14 +19,14 @@ const Home = () => (
         <img
           src={bgPath}
           alt="Company vehicle"
-          className="max-h-[90vh] min-w-full object-fill xl:min-w-[45vw] xl:max-h-[92vh]"
+          className="max-h-[90vh] min-w-full object-fill xl:min-w-[45vw] xl:max-h-[92vh] md:min-h-[90vh] md:min-w-[55vw]"
         />
         <div className="w-full h-full bg-[#000000a7] absolute top-0 xl:min-w-[45vw] md:hidden"></div>
       </div>
     </div>
     <div className="text-center my-12 text-2xl text-blueText font-semibold md:flex md:items-center md:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[45vw] md:flex-col">
       <div className="hidden md:block">
-        <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] 2xl:text-3xl xl:text-2xl">
+        <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] xl:text-2xl 2xl:text-3xl lg:max-w-[80%] lg:mx-auto md:mx-5">
           Welcome to Exploring SA Transport and Tours, your premier
           transportation and tourism provider in South Africa
         </h1>
@@ -34,7 +34,7 @@ const Home = () => (
       <h1 className="md:hidden">
         "Unlock the treasures of South Africa with us"
       </h1>
-      <button className="hidden md:block md:p-2 md:mt-4 md:text-lg sm:text-[1.3rem] md:opacity-95 md:hover:opacity-100 md:text-blueText md:font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold ">
+      <button className="hidden md:block md:p-2 md:mt-6 md:text-lg sm:text-[1.3rem] md:opacity-95 md:hover:opacity-100 md:text-blueText md:font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold">
         Explore our Services
       </button>
     </div>
