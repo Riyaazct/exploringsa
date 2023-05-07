ff7c264b614091987a2854daa46045c3c5b10bf3
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-07 02:13:46 +0200

    add animations to homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,12 +5,12 @@ const Home = () => (
   <div className="md:flex">
     <div className="relative">
       {/* TEXT CONTENT & BUTTON */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-offWhite text-center z-10 w-full max-w-[85%] md:top-52">
-        <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] 2xl:text-3xl xl:text-2xl md:hidden">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-offWhite text-center z-10 w-full max-w-[85%] md:top-52 ">
+        <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] 2xl:text-3xl xl:text-2xl md:hidden animate-fade-down animate-once animate-duration-1000 animate-ease-in">
           Welcome to Exploring SA Transport and Tours, your premier
           transportation and tourism provider in South Africa
         </h1>
-        <button className="p-2 mt-4 text-lg sm:text-[1.3rem] opacity-80 hover:opacity-100 text-blueText font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold md:hidden">
+        <button className="p-2 mt-4 text-lg sm:text-[1.3rem] opacity-80 hover:opacity-100 text-blueText font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold md:hidden animate-shake animate-twice animate-duration-[1000ms] animate-ease-in">
           Explore our Services
         </button>
       </div>
@@ -19,22 +19,22 @@ const Home = () => (
         <img
           src={bgPath}
           alt="Company vehicle"
-          className="max-h-[90vh] min-w-full object-fill xl:min-w-[45vw] xl:max-h-[92vh] md:min-h-[90vh] md:min-w-[55vw]"
+          className="max-h-[90vh] min-w-full object-fill xl:min-w-[45vw] xl:max-h-[92vh] md:min-h-[90vh] md:min-w-[55vw] "
         />
         <div className="w-full h-full bg-[#000000a7] absolute top-0 xl:min-w-[45vw] md:hidden"></div>
       </div>
     </div>
     <div className="text-center my-12 text-2xl text-blueText font-semibold md:flex md:items-center md:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[45vw] md:flex-col">
       <div className="hidden md:block">
-        <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] xl:text-2xl 2xl:text-3xl lg:max-w-[80%] lg:mx-auto md:mx-5">
+        <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] xl:text-2xl 2xl:text-3xl lg:max-w-[80%] lg:mx-auto md:mx-5 animate-fade-left animate-once animate-delay-200 animate-ease-in">
           Welcome to Exploring SA Transport and Tours, your premier
           transportation and tourism provider in South Africa
         </h1>
       </div>
-      <h1 className="md:hidden">
+      <h1 className="md:hidden animate-jump-in animate-once animate-duration-[2000ms] animate-ease-in">
         "Unlock the treasures of South Africa with us"
       </h1>
-      <button className="hidden md:block md:p-2 md:mt-6 md:text-lg sm:text-[1.3rem] md:opacity-95 md:hover:opacity-100 md:text-blueText md:font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold">
+      <button className="hidden md:block md:p-2 md:mt-6 md:text-lg sm:text-[1.3rem] md:opacity-95 md:hover:opacity-100 md:text-blueText md:font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold animate-fade-left animate-once animate-delay-200 animate-ease-in">
         Explore our Services
       </button>
     </div>
