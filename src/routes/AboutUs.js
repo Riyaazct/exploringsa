ff05a06cead7236668f386c34ab89bf526b8e57b
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-11 23:06:53 +0200

    add some more adjustments to about us page, our story section

--- src/routes/AboutUs.js
+++ src/routes/AboutUs.js
@@ -3,7 +3,7 @@ import Vision from "./Vision";
 
 const AboutUs = () => (
   <>
-    <div className="relative pt-16 md:mt-24 mb-52 md:mb-0 md:h-[80vh] lg:mb-[150px] 2xl:h-[90vh] ">
+    <div className="relative pt-16 md:mt-24 mb-52 sm:mb-36 md:mb-0 md:h-[80vh] lg:mb-[150px] 2xl:h-[90vh] ">
       <h1
         className="text-center my-12 text-2xl font-semibold text-blueText 
                      md:mt-0 md:mb-12 md:text-3xl
@@ -12,16 +12,16 @@ const AboutUs = () => (
         OUR STORY
       </h1>
       <div
-        className="flex flex-col-reverse justify-center items-center gap-5 md:flex-row md:gap-7 
+        className="flex flex-col-reverse justify-center items-center sm:mx-5 gap-5 sm:flex-row md:gap-7 
                       xl:mx-auto xl:max-w-[1650px] lg:max-w-[1000px] md:mx-auto md:max-w-[800px]
                       2xl:gap-10"
       >
         {/* TEXT */}
         <div
-          className="max-w-[80%] text-center rounded-lg mt-5 
+          className="max-w-[90%] sm:max-w-[45%] text-center rounded-lg mt-5 
                         md:max-w-[45%] lg:max-w-[50%] xl:max-w-[45%]"
         >
-          <p className="my-auto text-lg text-blueText font-semibold md:text-base md:px-4 lg:text-lg xl:text-2xl text-left">
+          <p className="my-auto text-blueText font-semibold md:text-[1.06rem] md:px-4 lg:text-lg xl:text-2xl sm:text-left">
             Exploring SA Transport and Tours was founded in 2018 by a
             team with over 20 years of experience in transportation
             and tourism. Our mission is to provide exceptional service
@@ -38,17 +38,17 @@ const AboutUs = () => (
           {/* Background div */}
           <div
             className="bg-lightGreen absolute h-full max-h-[540px] w-[100%] left-0 top-80 -z-10 
-                          aboutBg sm:left-10 sm:max-w-[90%] 
-                          md:-top-5 md:left-0 md:max-w-[85vw] md:max-h-[62vh] 
+                          aboutBg sm:max-w-[90%] 
+                          sm:-top-5 sm:left-0 md:max-w-[80vw] md:max-h-[57vh] 
                           lg:max-h-[72vh] lg:max-w-[80vw] 
                           xl:max-h-[76vh] 
                           2xl:max-w-[75vw] 2xl:max-h-[88vh]"
           />
         </div>
         {/* IMAGE */}
-        <div className="relative max-w-[75%] md:max-w-[48%] lg:max-w-[55%]">
+        <div className="relative sm:mb-10 md:max-w-[58%] lg:max-w-[55%]">
           <img
-            className=" m-auto rounded-lg md:max-h-[40%] lg:max-w-[480px] 
+            className=" m-auto sm:rounded-lg md:max-h-[40%] lg:max-w-[480px] 
                         xl:max-w-[550px] 2xl:max-w-[650px]"
             src="images/our story 2.jpg"
             alt="the company tour guide and owner with some tourists"
