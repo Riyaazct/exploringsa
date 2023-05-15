7b8520a5d9ae4d55ee0f479ad74f72f5b99429e9
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-15 23:58:05 +0200

    adjustments to image size

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,9 +5,9 @@ const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <div className="m-auto rounded-xl">
+    <div className="xl:bg-orange 2xl:bg-[#171717] w-screen h-full -mt-5">
       <img
-        className="w-screen"
+        className="w-auto h-screen mx-auto"
         src={data[currentIndex].image}
         alt={data[currentIndex].alt}
       />
