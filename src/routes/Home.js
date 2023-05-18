e5684d8423d8db180c46f7700337427bf5f2f525
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-18 20:37:44 +0200

    first commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,6 +4,8 @@ const bgPath = "/images/home.jpg";
 const Home = () => (
   <section className="md:flex">
     <div className="relative">
+      {/*  */}
+
       {/* TEXT CONTENT & BUTTON */}
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-offWhite text-center z-10 w-full max-w-[85%] md:top-52 ">
         <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] 2xl:text-3xl xl:text-2xl md:hidden animate-fade-down animate-once animate-duration-1000 animate-ease-in">
@@ -14,6 +16,8 @@ const Home = () => (
           Explore our Services
         </button>
       </div>
+      {/*  */}
+
       {/* IMAGE */}
       <div className="relative ">
         <img
@@ -24,6 +28,7 @@ const Home = () => (
         <div className="w-full h-full bg-[#000000a7] absolute top-0 xl:min-w-[45vw] md:hidden"></div>
       </div>
     </div>
+
     {/* TEXT CONTENT FOR SCREENS > 768px */}
     <div className="text-center my-12 text-2xl text-blueText font-semibold md:flex md:items-center md:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[45vw] md:flex-col">
       <div className="hidden md:block">
@@ -32,8 +37,9 @@ const Home = () => (
           transportation and tourism provider in South Africa
         </h1>
       </div>
+
       {/* TEXT CONTENT DISPLAYED ON THE MOBILE VIEW ONLY */}
-      <h1 className="text-[1.4rem] md:hidden animate-fade-right animate-once animate-duration-[1500ms] animate-delay-500 animate-ease-in-out">
+      <h1 className="text-[1.0rem] sm:text-2xl font-extrabold md:hidden animate-fade-right animate-once animate-duration-[1500ms] animate-delay-500 animate-ease-in-out up">
         "Unlock the treasures of South Africa with us"
       </h1>
       <button className="hidden md:block md:p-2 md:mt-6 md:text-lg sm:text-[1.3rem] md:opacity-95 md:hover:opacity-100 md:text-blueText md:font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold animate-jump-in animate-once animate-duration-[1500ms] animate-ease-in-out">
