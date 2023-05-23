dd4697879f0bf9c69ad7b87efb15c973d58e8e95
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-05-24 00:28:36 +0200

    changes to button on 2xl screen of homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -19,7 +19,7 @@ const Home = () => (
           <button
             className="p-2 mt-4 text-sm opacity-80 hover:opacity-100 text-blueText font-semibold 
             sm:text-[1.4rem] sm:p-3
-            xl:p-4 xl:text-2xl 2xl:font-extrabold md:hidden duration-500 animate-shake animate-twice animate-duration-[1000ms] animate-ease-in"
+            md:hidden duration-500 animate-shake animate-twice animate-duration-[1000ms] animate-ease-in"
           >
             Explore our Services
           </button>
@@ -41,9 +41,12 @@ const Home = () => (
     </div>
 
     {/* TEXT CONTENT FOR SCREENS > 768px */}
-    <div className="text-center my-12 text-2xl text-blueText font-semibold md:flex md:items-center md:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[45vw] md:flex-col">
+    <div className="text-center my-12 text-2xl text-blueText font-semibold md:flex md:items-center md:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[55vw] md:flex-col">
       <div className="hidden md:block">
-        <h1 className="font-semibold text-[1.2rem] sm:text-[1.3rem] xl:text-2xl 2xl:text-3xl lg:max-w-[80%] lg:mx-auto md:mx-5 animate-fade-left animate-once animate-delay-200 animate-ease-in">
+        <h1
+          className="font-semibold text-[1.2rem] sm:text-[1.3rem] xl:text-2xl 2xl:text-3xl lg:max-w-[80%] lg:mx-auto md:mx-5 
+                       animate-fade-left animate-once animate-delay-200 animate-ease-in"
+        >
           Welcome to Exploring SA Transport and Tours, your premier
           transportation and tourism provider in South Africa
         </h1>
@@ -53,8 +56,9 @@ const Home = () => (
       <h1 className="text-[1.0rem] sm:text-2xl font-extrabold md:hidden animate-fade-right animate-once animate-duration-[1500ms] animate-delay-500 animate-ease-in-out up">
         "Unlock the treasures of South Africa with us"
       </h1>
+      {/* BUTTON FOR SCREENS > 768 */}
       <Link to="/services#services">
-        <button className="hidden md:block md:p-2 md:mt-6 md:text-lg sm:text-[1.3rem] md:opacity-95 md:hover:opacity-100 md:text-blueText md:font-semibold xl:p-4 xl:text-2xl 2xl:font-extrabold animate-jump-in animate-once animate-duration-[1500ms] animate-ease-in-out">
+        <button className="hidden md:block md:p-2 md:mt-6 md:text-lg sm:text-[1.3rem] md:opacity-95 md:hover:opacity-100 md:text-blueText md:font-semibold xl:p-3 xl:text-xl 2xl:font-extrabold animate-jump-in animate-once animate-duration-[1500ms] animate-ease-in-out">
           Explore our Services
         </button>
       </Link>
