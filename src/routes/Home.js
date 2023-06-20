2cbd96fc5a104ffb416acbf8c96b28240bd7c07c
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-06-20 20:59:38 +0200

    adjust syling on homepage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -31,16 +31,20 @@ const Home = () => (
       <div className="relative ">
         <img
           src={bgPath}
-          style={{ width: "1080px", height: "1440px" }}
+          style={
+            window.innerWidth <= 768
+              ? { width: "100%", height: "100vh" }
+              : { width: "1080px", height: "1440px" }
+          }
           alt="Company vehicle"
-          className="w-full h-full max-h-[90vh] md:max-h-[100vh]"
+          className="w-full h-full max-h-[90vh]"
         />
         <div className="w-full h-full bg-[#000000a7] absolute top-0 xl:min-w-[45vw] lg:hidden"></div>
       </div>
     </div>
 
     {/* TEXT CONTENT FOR SCREEN LG PLUS */}
-    <div className="text-center my-12 text-2xl text-blueText font-semibold md:flex md:items-center md:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[55vw] md:flex-col">
+    <div className="text-center my-12 text-2xl text-blueText font-semibold lg:flex lg:items-center lg:m-auto xl:mx-auto xl:text-3xl 2xl:text-4xl xl:max-w-[55vw] lg:flex-col">
       <div className="hidden lg:block">
         <h1
           className="font-semibold text-[1.2rem] sm:text-[1.3rem] xl:text-2xl 2xl:text-3xl lg:max-w-[80%] lg:mx-auto md:mx-5 
@@ -51,8 +55,11 @@ const Home = () => (
         </h1>
       </div>
 
-      {/* TEXT CONTENT DISPLAYED ON THE MOBILE VIEW ONLY */}
-      <h1 className="text-[1.0rem] sm:text-2xl font-extrabold lg:hidden animate-fade-right animate-once animate-duration-[1500ms] animate-delay-500 animate-ease-in-out up">
+      {/* TEXT CONTENT DISPLAYED ON screens <= 768px */}
+      <h1
+        className="text-[1.0rem] sm:text-2xl font-extrabold 
+                     lg:hidden animate-fade-right animate-once animate-duration-[1500ms] animate-delay-500 animate-ease-in-out up"
+      >
         "Unlock the treasures of South Africa with us"
       </h1>
       {/* BUTTON FOR SCREENS LG PLUS */}
