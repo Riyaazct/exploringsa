0b65801d0212b0a51a23d2d0eecf3deea8ebdddc
Riyaazct <riyaazct1@gmail.com>
2023-11-06 20:59:12 +0200

    final adjustments to t6 carousel(I hope)

--- src/routes/Vehicles/Kombi.js
+++ src/routes/Vehicles/Kombi.js
@@ -8,17 +8,17 @@ import "react-image-gallery/styles/css/image-gallery.css";
 const Kombi = () => {
   return (
     <section className="my-16">
-      <div className="flex flex-col items-center w-full h-[60vh] p-5 mb-20 text-center lg:ml-12 lg:flex-row lg:gap-6 xl:gap-0 lg:p-10 lg:container 2xl:max-w-7xl xl:mx-auto 2xl:gap-16">
+      <div className="flex flex-col items-center w-full h-[60vh] p-5 mb-20 text-center lg:ml-12 md:flex-row lg:gap-6 xl:gap-0 lg:p-10 lg:container 2xl:max-w-7xl xl:mx-auto 2xl:gap-16">
         <div>
           <h2 className="p-3 mb-5 text-3xl lg:text-3xl 2xl:text-4xl">
             VW Transporter Kombi (T6)
           </h2>
-          <h3 className="mb-3 text-xl lg:pb-4 lg:text-xl 2xl:text-2xl">
+        </div>
+        <div className="mx-auto lg:max-w-5xl ">
+          <h3 className="mb-3 text-lg font-semibold lg:text-xl 2xl:text-2xl">
             Explore the VW Transporter, your perfect partner in
             tourism:
           </h3>
-        </div>
-        <div className="mx-auto lg:max-w-5xl ">
           <ol className="mx-auto text-left list-decimal list-inside lg:text-lg ">
             <li className="p-1 leading-tight 2xl:leading-7">
               Spacious Interior: Accommodate up to 9 passengers
@@ -52,7 +52,7 @@ const Kombi = () => {
           </ol>
         </div>
       </div>
-      <h4 className="py-5 text-lg text-center lg:text-3xl">
+      <h4 className="py-5 text-lg text-center md:text-2xl lg:text-3xl">
         "Embark on memorable journeys with the VW Transporter!"
       </h4>
       <ImageGallery items={images} autoPlay={true} />
