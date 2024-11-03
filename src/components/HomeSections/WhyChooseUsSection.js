a1c88f3503030013c60c5e1d2dfa27d4cf822068
Riyaazct <riyaazct1@gmail.com>
2024-11-03 09:51:17 +0200

    update section

--- src/components/HomeSections/WhyChooseUsSection.js
+++ src/components/HomeSections/WhyChooseUsSection.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WhyChooseUsSection = () => {
   return (
-    <section className="md:grid md:grid-cols-2 lg:grid-cols-[25%_35%_40%] lg:h-[70vh] lg:grid-rows-[50%_50%]">
+    <section className="md:grid md:grid-cols-2 lg:grid-cols-[20%_35%_45%] lg:h-[95vh] xl:h-screen xl:grid-cols-[18%_49%_33%]">
       {/* TITLE */}
       <div className="w-full p-16 m-auto text-center text-bluebg md:col-span-2 md:max-w-4xl sm:max-w-xl lg:col-span-1 lg:text-right lg:p-8">
         <h2 className="text-2xl md:text-orange lg:text-darkGreen md:text-3xl xl:text-danger lg:text-4xl lg:w-56 lg:ml-auto ">
@@ -23,23 +23,23 @@ const WhyChooseUsSection = () => {
         <img
           src="images/sections/why choose us.jpg"
           alt="Tourists standing behind the cape of goodhope sign board "
-          className="w-full h-full lg:object-cover"
+          className="w-full h-full xl:max-h-[900px] md:hidden lg:block"
         />
 
-        {/* IMAGE FOR LG viewport */}
+        {/* IMAGE FOR MD viewport */}
         <img
           src="images/sections/why choose us - md.jpg"
           alt="Tourists standing behind the cape of goodhope sign board "
-          className="hidden w-full h-full "
+          className="hidden object-cover w-full h-full md:block lg:hidden"
         />
-        <div className="absolute top-0 w-full h-full bg-black bg-opacity-10 " />
+        <div className="absolute top-0 w-full h-full bg-black bg-opacity-0" />
       </div>
 
       {/* CARDS */}
 
       {/* CARD 1 */}
 
-      <div className="bg-[#4CAF50] h-full w-full p-14 text-center text-[#212121] lg:px-5 lg:order-first lg:text-left">
+      <div className="bg-[#4CAF50] h-full w-full p-14 text-center text-[#212121] lg:px-5 lg:order-first lg:text-left lg:flex">
         <div className="m-auto sm:max-w-md">
           <img
             className="w-24 h-24 m-auto mb-2 lg:h-14 lg:w-14 lg:mx-0"
@@ -48,10 +48,10 @@ const WhyChooseUsSection = () => {
           />
           {/* CARD TEXT */}
           <div>
-            <h4 className="px-12 text-xl font-extrabold leading-tight lg:text-base lg:px-4 lg:pl-0">
+            <h4 className="px-12 text-xl font-extrabold leading-tight lg:text-base lg:px-4 lg:pl-0 xl:text-lg">
               Trusted Experts in Transportation:
             </h4>
-            <p className="px-12 mt-1 mb-4 lg:text-sm lg:px-0">
+            <p className="px-12 mt-1 mb-4 lg:text-sm lg:px-0 xl:text-base">
               Years of experience trusted by clients for charter
               services, airport transfers, and corporate travel.
             </p>
@@ -60,7 +60,7 @@ const WhyChooseUsSection = () => {
       </div>
 
       {/* CARD 2 */}
-      <div className="bg-[#FFF3E0] h-full w-full p-14 text-center text-[#212121] lg:px-5 lg:text-left">
+      <div className="bg-[#FFF3E0] h-full w-full p-14 text-center text-[#212121] lg:px-5 lg:text-left lg:flex">
         <div className="m-auto sm:max-w-md">
           <img
             className="w-24 h-24 m-auto mb-2 lg:h-14 lg:w-14 lg:mx-0"
@@ -69,10 +69,10 @@ const WhyChooseUsSection = () => {
           />
           {/* CARD TEXT */}
           <div>
-            <h4 className="px-12 text-xl font-extrabold leading-tight lg:leading-6 lg:text-base lg:px-4 lg:pl-0">
+            <h4 className="px-12 text-xl font-extrabold leading-tight lg:leading-6 lg:text-base lg:px-4 lg:pl-0 xl:text-lg">
               Tailored Tours for Every Traveller:
             </h4>
-            <p className="px-12 mt-1 mb-4 lg:text-sm lg:px-0">
+            <p className="px-12 mt-1 mb-4 lg:text-sm lg:px-0 xl:text-base">
               Personalized tours designed to meet the specific needs
               of every traveller, whether for business or leisure.
             </p>
@@ -81,20 +81,20 @@ const WhyChooseUsSection = () => {
       </div>
 
       {/* CARD 3 */}
-      <div className="h-full md:grid md:grid-cols-2 md:col-span-2 lg:col-span-1 ">
-        <div className="bg-[#1B5E20] h-full w-full p-14 text-center text-[#E1E1E1] lg:px-0 lg:pt-10 lg:text-left">
-          <div className="m-auto sm:max-w-md">
+      <div className="h-full md:grid md:grid-cols-2 md:col-span-2 lg:col-span-1">
+        <div className="bg-[#1B5E20] h-full w-full p-14 text-center text-[#E1E1E1] lg:px-0 lg:pt-10 lg:text-left lg:flex">
+          <div className="m-auto sm:max-w-md lg:px-5">
             <img
-              className="m-auto -mb-2 h-28 lg:h-14 lg:w-14 lg:mx-0"
+              className="m-auto -mb-2 h-28 lg:h-[4.5rem] lg:w-18 lg:mx-0"
               src="images/icons/sections/van.svg"
               alt="bags of luggage packed and ready for departure"
             />
             {/* CARD TEXT */}
             <div>
-              <h4 className="px-12 text-xl font-extrabold leading-tight lg:text-base lg:px-2 lg:pl-0">
+              <h4 className="px-12 text-xl font-extrabold leading-tight lg:text-base lg:px-2 lg:pl-0 xl:text-lg">
                 Reliable Fleet of Vehicles:
               </h4>
-              <p className="m-auto mt-1 mb-4 lg:text-sm">
+              <p className="m-auto mt-1 mb-4 lg:text-sm xl:text-base">
                 Modern, safe, and comfortable vehicles, including the
                 VW Crafter Minibus, tailored for group or individual
                 transport.
@@ -104,20 +104,20 @@ const WhyChooseUsSection = () => {
         </div>
 
         {/* CARD 4 */}
-        <div className="bg-[#FFE0B2] h-full w-full p-14 text-center text-[#212121] lg:px-0 lg:pt-6 lg:text-left">
-          <div className="m-auto sm:max-w-md">
+        <div className="bg-[#FFE0B2] h-full w-full p-14 text-center text-[#212121] lg:px-0 lg:pt-10 lg:text-left lg:flex">
+          <div className="m-auto sm:max-w-md lg:px-5">
             <img
-              className="w-24 h-24 m-auto mb-2 lg:h-14 lg:w-14 lg:mx-0"
+              className="w-24 h-24 m-auto mb-2 lg:h-14 lg:w-14 lg:mx-0 lg:mb-0"
               src="images/icons/sections/headset.svg"
               alt="A call centre agent headset icon"
             />
 
             {/* CARD TEXT */}
-            <div>
-              <h4 className="px-12 text-xl font-extrabold leading-tight lg:text-base lg:px-2 lg:pl-0">
+            <div className="">
+              <h4 className="px-12 text-xl font-extrabold leading-tight lg:text-base lg:px-2 lg:pl-0 xl:text-lg">
                 Exceptional Customer Service:
               </h4>
-              <p className="m-auto mt-1 mb-4 lg:text-sm">
+              <p className="m-auto mt-1 mb-4 lg:text-sm xl:text-base">
                 Dedicated support available 24/7 to assist with
                 bookings, inquiries, and special requests, ensuring a
                 seamless travel experience.
