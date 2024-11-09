963ef3e4abadb0bf9e63eae156b547ffb8b276a7
Riyaazct <riyaazct1@gmail.com>
2024-11-09 22:43:07 +0200

    adjust styling and lik buttons to specific vehicle

--- src/components/HomeSections/OurFleetSection.js
+++ src/components/HomeSections/OurFleetSection.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import fleetSectionData from "../../data/fleetSectionData";
 
 const OurFleetSection = () => {
@@ -6,35 +8,45 @@ const OurFleetSection = () => {
       {/**/}
 
       {/* TITLE */}
-      <h2 className="p-10">
+      <h2 className="p-12 text-2xl font-extrabold text-center text-bluebg max-w-[80%] m-auto">
         Our Premium Fleet – Tailored to Your Needs
       </h2>
 
       {/* CARDS */}
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col w-full gap-2 m-auto md:flex-row md:flex-wrap xl:max-w-7xl">
         {fleetSectionData.map(
-          ({ image, alt, title, description, button }, i) => (
+          ({ image, alt, title, description, button, route }, i) => (
             <div
-              className="m-6 drop-shadow-2xl bg-[#F2F2F2] flex flex-col items-center"
+              className="drop-shadow-2xl bg-[#F2F2F2] flex flex-col items-center m-auto max-w-sm"
               key={i}
             >
               {/*  */}
 
               {/* IMAGE AND OVERLAY */}
               <div className="relative">
-                <img src={image} alt={alt} />
+                <img
+                  src={image}
+                  alt={alt}
+                  className="w-full h-auto"
+                />
                 <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-5" />
               </div>
 
               {/* TITLE */}
-              <h3 className="p-10 mt-10">{title}</h3>
+              <h3 className="mt-5 text-2xl font-extrabold text-blueText max-w-[95%]">
+                {title}
+              </h3>
 
               {/* DESCRIPTION */}
-              <div className="p-10 mt-10">{description}</div>
+              <div className="mt-5 max-w-[75%] min-h-[16rem]">
+                {description}
+              </div>
 
               {/* BUTTON */}
-              <button className="w-full max-w-xs my-10">
-                {button}
+              <button className="w-full max-w-xs p-4 my-10 text-lg font-extrabold border-none text-blueText">
+                <Link to={route} target="_blank">
+                  {button}
+                </Link>
               </button>
             </div>
           )
--- src/data/fleetSectionData.js
+++ src/data/fleetSectionData.js
@@ -6,26 +6,39 @@ const fleetSectionData = [
       "22 Seater Minibus – Ideal for Group Charters and Tourist Travel",
     description: (
       <ul className="list-disc">
-        <li>
-          <span>Best for: </span>Large group charters, tourist
-          journeys, and airport transfers for big groups.
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Best for:{" "}
+          </span>
+          Large group charters, tourist journeys, and airport
+          transfers for big groups.
         </li>
-        <li>
-          <span>Capacity:</span>Up to 22 passengers
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Capacity:
+          </span>
+          Up to 22 passengers
         </li>
-        <li>
-          <span>Features: </span>Spacious interior, air-conditioned,
-          ideal for sightseeing tours, and group transportation.
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Features:{" "}
+          </span>
+          Spacious interior, air-conditioned, ideal for sightseeing
+          tours, and group transportation.
         </li>
-        <li>
-          <span>Description: </span> The ultimate choice for your
-          charter services and group travel. Spacious, comfortable,
-          and perfect for unforgettable tourist journeys, whether it's
-          airport transfers, day trips, or long-distance charters.
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Description:{" "}
+          </span>{" "}
+          The ultimate choice for your charter services and group
+          travel. Spacious, comfortable, and perfect for unforgettable
+          tourist journeys, whether it's airport transfers, day trips,
+          or long-distance charters.
         </li>
       </ul>
     ),
     button: "Book Your Group Transfer",
+    route: "/crafter01",
   },
   {
     image: "images/sections/7 seater.png",
@@ -34,26 +47,38 @@ const fleetSectionData = [
       "7 Seater Transporter – Perfect for Family Adventures and Small Group Transfers",
     description: (
       <ul className="list-disc">
-        <li>
-          <span>Best for:</span> Airport transfers, private tours,
-          family adventures, and small corporate group transportation.{" "}
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Best for:
+          </span>{" "}
+          Airport transfers, private tours, family adventures, and
+          small corporate group transportation.{" "}
         </li>
-        <li>
-          <span>Capacity: </span> Up to 7 passengers
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Capacity:{" "}
+          </span>{" "}
+          Up to 7 passengers
         </li>
-        <li>
-          <span>Features: </span>Air-conditioned, flexible seating,
-          ample luggage space.
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Features:{" "}
+          </span>
+          Air-conditioned, flexible seating, ample luggage space.
         </li>
-        <li>
-          <span>Description: </span>Explore Cape Town with comfort and
-          style. Ideal for family tours, airport transfers, and small
-          corporate groups, the 7-seater Transporter offers
-          reliability and a smooth ride for any adventure.
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Description:{" "}
+          </span>
+          Explore Cape Town with comfort and style. Ideal for family
+          tours, airport transfers, and small corporate groups, the
+          7-seater Transporter offers reliability and a smooth ride
+          for any adventure.
         </li>
       </ul>
     ),
     button: "Reserve for Your Private Tour",
+    route: "/kombi01",
   },
   {
     image: "images/sections/13 seater.png",
@@ -62,27 +87,38 @@ const fleetSectionData = [
       "13 Seater Minibus – Executive Comfort for Corporate and VIP Transfers",
     description: (
       <ul className="list-disc">
-        <li>
-          <span>Best for:</span> Executive airport transfers, VIP
-          transportation, and private corporate events.{" "}
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Best for:
+          </span>{" "}
+          Executive airport transfers, VIP transportation, and private
+          corporate events.{" "}
         </li>
-        <li>
-          <span>Capacity: </span>Up to 13 passengers
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Capacity:{" "}
+          </span>
+          Up to 13 passengers
         </li>
-        <li>
-          <span>Features: </span>Premium seating, air-conditioned,
-          luxurious ride.
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Features:{" "}
+          </span>
+          Premium seating, air-conditioned, luxurious ride.
         </li>
-        <li>
-          <span>Description: </span> Experience unparalleled style and
-          comfort with our 13-seater minibus. Designed for executive
-          travel, airport transfers, and private chauffeured services,
-          this vehicle is perfect for ensuring a smooth and
-          sophisticated ride.
+        <li className="py-1 text-sm text-blueText">
+          <span className="mr-1 text-base font-extrabold ">
+            Description:{" "}
+          </span>{" "}
+          Experience unparalleled style and comfort with our 13-seater
+          minibus. Designed for executive travel, airport transfers,
+          and private chauffeured services, this vehicle is perfect
+          for ensuring a smooth and sophisticated ride.
         </li>
       </ul>
     ),
     button: "Book Executive Transfer",
+    route: "/quantum01",
   },
 ];
 
